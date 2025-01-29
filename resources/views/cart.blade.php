282e2faaeae0162fdbabadffc0a3cbb524e00dda
Yeminthu960724 <yepaing22222@gmail.com>
2025-01-29 11:57:46 +0900

    Timeline icon added

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -17,6 +17,7 @@ $place = ['大阪', '京都', '奈良', '神戸', '和歌山', '滋賀', '兵庫
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
